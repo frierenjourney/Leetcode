e33cf1aee96e015798b20535dfcc38ebdc2efd0f
frierenjourney <samjainiam@gmail.com>
2026-07-01 14:49:18 +0530

    Time: 0 ms (100%), Space: 15.8 MB (5.59%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,9 +2,32 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-      next_permutation(nums.begin(),nums.end());
-      for(int i=0;i<n;i++){
-        cout<<nums[i]<<","<<endl;
-      }
+
+        int index = -1;
+
+        // Find the first decreasing element from the right
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+
+        // If no such element exists, reverse the whole array
+        if (index == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        // Find the next greater element from the right
+        for (int i = n - 1; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                swap(nums[i], nums[index]);
+                break;
+            }
+        }
+
+        // Reverse the remaining part
+        reverse(nums.begin() + index + 1, nums.end());
     }
 };
