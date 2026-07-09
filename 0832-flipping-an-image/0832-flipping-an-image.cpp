1c16d6682931c7b734f566f63a6f83ef508bbd28
frierenjourney <samjainiam@gmail.com>
2026-07-09 18:47:18 +0530

    Time: 0 ms (100%), Space: 12.7 MB (36.02%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.cpp
+++ 0832-flipping-an-image/0832-flipping-an-image.cpp
@@ -4,9 +4,7 @@ public:
         int n = image.size();
         for(int i=0;i<n;i++){
                 reverse(image[i].begin(),image[i].end());
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+                for(int j=0;j<n;j++){
                 image[i][j]=image[i][j]^1;
             }
         }
