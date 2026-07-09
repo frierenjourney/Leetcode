2ccf480e7e21a511819f0b16f36eba832689ebde
frierenjourney <samjainiam@gmail.com>
2026-07-10 03:54:30 +0530

    Time: 103 ms (5.32%), Space: 20.7 MB (76.99%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,28 +1,64 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<int> x(matrix[0].size(),0);
-        vector<int> y(matrix.size(),0);
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(matrix[i][j]==0){
-                    x[j]=1;
-                    y[i]=1;
+    //     vector<int> x(matrix[0].size(),0);
+    //     vector<int> y(matrix.size(),0);
+    //     for(int i=0;i<matrix.size();i++){
+    //         for(int j=0;j<matrix[i].size();j++){
+    //             if(matrix[i][j]==0){
+    //                 x[j]=1;
+    //                 y[i]=1;
+    //             }
+    //         }
+    //     }
+    //     for(int i=0;i<matrix.size();i++){
+    //         for(int j=0;j<matrix[i].size();j++){
+    //             if(x[j] || y[i]){
+    //                 matrix[i][j]=0;
+    //             }
+    //         }
+    //     }
+    // for(int i=0;i<matrix.size();i++){
+    //         for(int j=0;j<matrix[i].size();j++){
+    //             cout<<matrix[i][j];
+    //         }
+    //         cout<<endl;
+    //     }
+    int col0=1;
+    for(int i = 0;i<matrix.size();i++){
+        for(int j=0;j<matrix[0].size();j++){
+            if(matrix[i][j]==0){
+                matrix[i][0]=0;
+                if(j!=0){
+                    matrix[0][j]=0;
+                }
+                else{
+                    col0=0;
                 }
             }
         }
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[i].size();j++){
-                if(x[j] || y[i]){
-                    matrix[i][j]=0;
-                }
+    }
+    for(int i = 1;i<matrix.size();i++){
+        for(int j=1;j<matrix[0].size();j++){
+            if(matrix[i][0]==0 || matrix[0][j]==0){
+                matrix [i][j]=0;
             }
         }
+    }
+    if(matrix[0][0]==0){
+        for(int i=0;i<matrix[0].size();i++){
+            matrix[0][i]=0;
+        }
+    }
+    if(col0==0){
+        for(int i=0;i<matrix.size();i++){
+            matrix[i][0]=0;
+        }
+    }
     for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[i].size();j++){
                 cout<<matrix[i][j];
             }
-            cout<<endl;
-        }
+    }
     }
 };
