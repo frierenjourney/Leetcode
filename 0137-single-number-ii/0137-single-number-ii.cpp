af5c85892b7c7ddb31fb0048c4e7abd19a164bbd
frierenjourney <samjainiam@gmail.com>
2026-07-14 03:13:50 +0530

    Time: 0 ms (100%), Space: 13.2 MB (99.37%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int count  = 0;
-        for(int i=0;i<nums.size();i++){
-            for(int j = 0;j<nums.size();j++){
-                if(nums[i]==nums[j])count++;
-                if(count>1){
-                    count = 0;
-                    break;
-                }
-            }
-            if(count == 1){
-                return nums[i];
-            }
+    //     int count  = 0;
+    //     for(int i=0;i<nums.size();i++){
+    //         for(int j = 0;j<nums.size();j++){
+    //             if(nums[i]==nums[j])count++;
+    //             if(count>1){
+    //                 count = 0;
+    //                 break;
+    //             }
+    //         }
+    //         if(count == 1){
+    //             return nums[i];
+    //         }
+    //     }
+    // return 0;
+    sort(nums.begin(),nums.end());
+    int j=1;
+    int i = 0;
+    if(nums.size()==1){
+        return nums[i];
+    }
+    while(i<nums.size()){
+        if(nums[i]==nums[j]){
+            i+=3;
+            j+=3;
+            if(i==nums.size()-1)return nums[i];
         }
-    return 0;
+        else return nums[i];
+    }
+    return false;
     }
 };
