acb2b9a8564d6c426ac118b5753b83693a5acfaf
frierenjourney <samjainiam@gmail.com>
2026-07-14 18:36:30 +0530

    Time: 0 ms (100%), Space: 13.3 MB (79.18%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // for(int i = 0;i<matrix.size();i++){
+        //     for(int j = 0;j<matrix[0].size();j++){
+        //         if(matrix[i][j]==target)return true;
+        //     }
+        // }
+        // return false;
         for(int i = 0;i<matrix.size();i++){
-            for(int j = 0;j<matrix[0].size();j++){
-                if(matrix[i][j]==target)return true;
+            if(matrix[i][0]<=target && matrix[i][matrix[0].size()-1]>=target){
+                for(int j = 0;j<matrix[0].size();j++){
+                    if(matrix[i][j]==target){
+                        return true;
+                    }
+                }
+                return false;
             }
         }
         return false;
