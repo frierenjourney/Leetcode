2503c0f952bf4fc5b69215ddbdd03e9b44cef299
frierenjourney <samjainiam@gmail.com>
2026-07-14 18:41:25 +0530

    Time: 0 ms (100%), Space: 13.3 MB (45.07%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -7,11 +7,31 @@ public:
         //     }
         // }
         // return false;
+        // for(int i = 0;i<matrix.size();i++){
+        //     if(matrix[i][0]<=target && matrix[i][matrix[0].size()-1]>=target){
+        //         for(int j = 0;j<matrix[0].size();j++){
+        //             if(matrix[i][j]==target){
+        //                 return true;
+        //             }
+        //         }
+        //         return false;
+        //     }
+        // }
+        // return false;
         for(int i = 0;i<matrix.size();i++){
             if(matrix[i][0]<=target && matrix[i][matrix[0].size()-1]>=target){
+                int left = 0;
+                int right =  matrix[0].size()-1;
                 for(int j = 0;j<matrix[0].size();j++){
-                    if(matrix[i][j]==target){
+                    int mid = (left+right)/2;
+                    if(matrix[i][mid]==target){
                         return true;
+                    }
+                    else if(matrix[i][mid]>target){
+                        right=mid-1;
+                    }
+                    else{
+                        left=mid+1;
                     }
                 }
                 return false;
