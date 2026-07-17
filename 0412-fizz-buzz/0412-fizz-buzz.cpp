cf936c77d2fad56308cc6cdc160d7a9f0fba2688
frierenjourney <samjainiam@gmail.com>
2026-07-18 03:00:12 +0530

    Time: 2 ms (44.22%), Space: 11.9 MB (17.94%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+    vector<string> x;
+    for(int i=1;i<=n;i++){
+        if(i%3==0){
+            if(i%5==0){
+                x.push_back("FizzBuzz");
+            }
+            else
+            x.push_back("Fizz");
+        }
+        else if(i%5==0){
+                x.push_back("Buzz");
+            }
+        else
+         x.push_back(to_string(i));
+       }
+       return x;
+    }
+};
