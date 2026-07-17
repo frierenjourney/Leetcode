aeb3bd06ea826f232505b2a075065b5feae9b204
frierenjourney <samjainiam@gmail.com>
2026-07-18 03:33:48 +0530

    Time: 0 ms (100%), Space: 7.8 MB (97.04%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
     int findComplement(int num) {
-        vector<int> x;
+        // vector<int> x;
+        // int y = num;
+        // while(y >= 2){
+        //     x.push_back(y%2);
+        //     y = y/2;
+        // }
+        // x.push_back(y);
+        // for(int i = 0;i<x.size();i++){
+        //     if(x[i]==0)x[i]=1;
+        //     else x[i]=0;
+        // }
+        // long long sum=0;
+        // long long k = 1;
+        // for(int i = 0;i<x.size();i++){
+        //     sum+=x[i]*k;
+        //     k = k*2;
+        // }
+        // return sum;
         int y = num;
-        while(y >= 2){
-            x.push_back(y%2);
-            y = y/2;
+        long long x=0;
+        long long z=1;
+        while(y > 0){
+            int bit = y%2;
+            if(bit==0){
+                x+=z;
+            }
+            y/=2;
+            z*=2;
         }
-        x.push_back(y);
-        for(int i = 0;i<x.size();i++){
-            if(x[i]==0)x[i]=1;
-            else x[i]=0;
-        }
-        long long sum=0;
-        long long k = 1;
-        for(int i = 0;i<x.size();i++){
-            sum+=x[i]*k;
-            k = k*2;
-        }
-        return sum;
+        return x;
     }
 };
