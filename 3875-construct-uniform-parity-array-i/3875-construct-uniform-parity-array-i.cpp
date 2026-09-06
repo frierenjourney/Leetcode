d9c19b7046d6e88752b57ef587296776bf4d2984
frierenjourney <samjainiam@gmail.com>
2026-09-07 02:27:35 +0530

    Time: 0 ms (100%), Space: 30.3 MB (30%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    //     int i=0;
+    //     count=0;
+    //     while(count<0 && i<nums.size()){
+    //         if(nums[i]%2!=0)count++;
+    //         i++;
+    //     }
+    // if(i==nums.size()-1)return true;
+
+    }
+};
