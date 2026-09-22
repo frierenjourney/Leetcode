f0f3f8de0dcf27d098da9fcaf35cc04050f79d0b
frierenjourney <samjainiam@gmail.com>
2026-09-22 20:12:39 +0530

    Time: 0 ms (100%), Space: 12.1 MB (24.36%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string y = strs[0];
-        for(int i=0;i<strs.size();i++){
-            string x = ""; 
-            int j=0;
-            while(j<strs[i].size()){
-            if(strs[i][j]==y[j])
-            x.push_back(y[j]);
-            else break;
-            j++;
-            }
-            y = x;
+        // string y = strs[0];
+        // for(int i=0;i<strs.size();i++){
+        //     string x = ""; 
+        //     int j=0;
+        //     while(j<strs[i].size()){
+        //     if(strs[i][j]==y[j])
+        //     x.push_back(y[j]);
+        //     else break;
+        //     j++;
+        //     }
+        //     y = x;
 
-            if(y == "")
+        //     if(y == "")
+        //         return "";
+        // }
+        // return y;
+        string s = strs[0];
+        for(int i=0;i<strs.size();i++){
+            string x = "";
+            for(int j=0;j<strs[i].size();j++){
+                if(strs[i][j]==s[j]){
+                    x.push_back(s[j]);
+                }
+                else break;
+            }
+            s = x;
+            if(s == "")
                 return "";
         }
-        return y;
+        return s;
     }
 };
