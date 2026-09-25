15071bf4ffcf2a578feb85f70184a425b4d8b0f9
frierenjourney <samjainiam@gmail.com>
2026-09-25 09:16:15 +0530

    Time: 0 ms (100%), Space: 30.9 MB (81.78%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -4,14 +4,10 @@ public:
         for(int i=0;i<nums.size();i++){
             int x = nums[i];
             int sum=0;
-            if(x>9){
-                while(x>9){
+                while(x>0){
                     sum+=x%10;
                     x=x/10;
                 }
-                sum+=x;
-            }
-            else sum = x;
             if(sum==i)return i;
         }
         return -1;
