6cd0de27c9ebd69b34a7681876d44d765430fd16
frierenjourney <samjainiam@gmail.com>
2026-09-26 14:09:25 +0530

    Time: 0 ms (100%), Space: 8.4 MB (4.25%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countSegments(string s) {
+        int count = 0;
+        stringstream ss(s);
+        string word;
+        while(ss>>word)count++;
+        return count;
+    }
+};
